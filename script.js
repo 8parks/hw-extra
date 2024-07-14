b8c184e373fa794c62508915efb20042bfbf44ac
8parks <trxye.522@gmail.com>
2024-07-14 16:18:44 +0900

    [feat] hw3

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const inputButton = document.getElementById("inputButton"); 
-    const textInput = document.getElementaryById("textInput");
-    const messages = doument.querySelectorAll(".message");
+    const calculateButton = document.getElementById("calculateButton"); 
+    const numInput1 = document.getElementById("numInput1");
+    const numInput2 = document.getElementById("numInput2");
+    const messages = document.querySelectorAll(".message");
    
-   inputButton.addEventListener("click", function () {
-      // mainbox의 표시 상태를 토글
+   calculateButton.addEventListener("click", function () {
+        const num1 = parseInt(numInput1.value, 10)||0;
+        const num2 = parseInt(numInput2.value, 10)||0;
+        let increment = 1;
+        
         messages.forEach(function (message) {
-            message.textContent = textInput.value;
+            const adjustedNum1 = num1 + increment;
+            const adjustedNum2 = num2 + increment;
+            const result = adjustedNum1 * adjustedNum2;
+            message.textContent = `${adjustedNum1} X ${adjustedNum2} = ${result}`; 
+            increment++;
         });
 
-        textInput.value = "";
     });
 });
    
